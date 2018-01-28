017d91ce9948ec4e2c3da41e9287b168408b10c3
Michael Fridman <michaelfridman@me.com>
2018-01-28 13:19:23 -0500

    Update godoc

--- doc.go
+++ doc.go
@@ -1,15 +1,15 @@
 /*
-Package srfax is a Go client for the SRFax API service, supporting all
-POST operations. The official SRFax API docs can be found here:
+Package srfax is a Go client library for interacting with the SRFax API service,
+supporting all POST operations. The official SRFax API docs can be found here:
 
 https://www.srfax.com/developers/our-fax-api/
 
-There are a few notable differences between the official docs and this
-client as a result of variable types being returned.
+There are a few notable differences between the official docs and this client:
 
-See note about error handling: https://github.com/mfridman/srfax#srfax
+1.	As a result of variable types being returned in the Result field, see note about error handling: https://github.com/mfridman/srfax#srfax
 
-The response format is JSON-only. Despite the API returning both XML
-and JSON, it's a design decision (for now) to support JSON only.
+2.	Response format supports JSON-only. Despite the API being able to return both XML
+and JSON, it is a design decision (for now) to support JSON only. As a result,
+sResponseFormat is not available as an optional parameter.
 */
 package srfax
