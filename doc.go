f6f99f3ec2dbf0ba703801df35afa5d92b37caba
Michael Fridman <michaelfridman@me.com>
2018-01-28 13:05:28 -0500

    Update godoc

--- doc.go
+++ doc.go
@@ -5,9 +5,11 @@ POST operations. The official SRFax API docs can be found here:
 https://www.srfax.com/developers/our-fax-api/
 
 There are a few notable differences between the official docs and this
-client as a result of variable types coming back from the server. See README file.
+client as a result of variable types being returned.
+
+See note about error handling: https://github.com/mfridman/srfax#srfax
 
 The response format is JSON-only. Despite the API returning both XML
-and JSON, it's a design decision (for now) to support JSON only respones.
+and JSON, it's a design decision (for now) to support JSON only.
 */
 package srfax
