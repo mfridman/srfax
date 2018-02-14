799d90c9040c3af3454b5603e3907243b03f63a8
Michael Fridman <michaelfridman@me.com>
2018-02-13 22:41:14 -0500

    Refactor CheckAuth and add method to count pages

--- client.go
+++ client.go
@@ -1,7 +1,8 @@
 package srfax
 
 import (
-	"strings"
+	"fmt"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -14,12 +15,13 @@ type ClientCfg struct {
 
 // NewClient returns an SRFax client based on configuration.
 func NewClient(c ClientCfg) (*Client, error) {
-	if c.ID == 0 {
-		return nil, errors.New("must specify access id. User Number")
+	if c.ID <= 0 {
+		return nil, errors.New("access id (access id) must be a positive number")
 	}
 	if c.Pwd == "" {
-		return nil, errors.New("must specify access pwd. Password on the users account")
+		return nil, errors.New("access password cannot be blank")
 	}
+
 	return &Client{AccessID: c.ID, AccessPwd: c.Pwd}, nil
 }
 
@@ -29,16 +31,74 @@ type Client struct {
 	AccessPwd string `json:"access_pwd"`
 }
 
-// CheckAuth checks whether client is able to authenticate. A wrapper around the
-// GetFaxUsage method. Used for convenience to quickly check if access ID & Pwd are valid.
+// CheckAuth verifies client's ability to authenticate with SRFax. It is a wrapper around the
+// GetFaxUsage method. Convenience method to quickly check if access ID & Pwd are valid.
 func (c *Client) CheckAuth() (bool, error) {
-	resp, err := c.GetFaxUsage()
+	_, err := c.GetFaxUsage()
 	if err != nil {
 		return false, err
 	}
 
-	if strings.ToLower(resp.Status) != "success" {
-		return false, errors.Errorf("No Result errors but Status was not Success: [%v]", resp.Status)
-	}
 	return true, nil
+}
+
+// Usage reports the total pages used and the date range.
+type Usage struct {
+	TotalPages int
+	AccessID   int
+	StartDate  time.Time
+	EndDate    time.Time
+}
+
+func (u *Usage) String() string {
+	return fmt.Sprintf("Account %d used %d pages from %v to %v", u.AccessID, u.TotalPages, u.StartDate.Format("Jan 02 2006"), u.EndDate.Format("Jan 02 2006"))
+}
+
+// PagesThisMonth reports pages used by ALL users of the account in the current month based on reset day.
+// Each account will have a unique reset day.
+// BUG(mf): This may be an issue with the SRFax API, need to contact support.
+// "sIncludeSubUsers": "Y" doesn't return sub user accounts making the page count lower. This should
+// reflect the Group Fax Usage in the Account Summary GUI.
+func (c *Client) PagesThisMonth(day int) (*Usage, error) {
+
+	var start, end time.Time
+	layout := "20060102"
+	now := time.Now()
+
+	// current date before the reset day for the current month
+	reset := time.Date(now.Year(), time.Month(int(now.Month())), day, 0, 0, 0, 0, time.Local)
+	switch now.Before(reset) {
+	case true:
+		// in the current month, BEFORE the reset date
+		end = reset
+		start = end.AddDate(0, -1, 0) // go back one month from end date
+	case false:
+		// in the current month, AFTER the reset date
+		end = time.Date(now.Year(), time.Month(int(now.Month()+1)), day, 0, 0, 0, 0, time.Local)
+		start = end.AddDate(0, -1, 0) // go back one month from end date
+	}
+
+	opts := GetFaxUsageOpts{
+		IncludeSubUsers: "Y",
+		Period:          "RANGE",
+		StartDate:       start.Format(layout),
+		EndDate:         end.Format(layout),
+	}
+
+	resp, err := c.GetFaxUsage(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	u := Usage{
+		StartDate: start,
+		EndDate:   end,
+		AccessID:  c.AccessID,
+	}
+
+	for i := range resp.Result {
+		u.TotalPages += resp.Result[i].NumberOfPages
+	}
+
+	return &u, nil
 }
