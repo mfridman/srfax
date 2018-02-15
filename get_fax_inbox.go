be70e10909c9c988af13a3d638ddf670c9bf740f
Michael Fridman <michaelfridman@me.com>
2018-02-14 23:55:10 -0500

    Added validation to inbox options

--- get_fax_inbox.go
+++ get_fax_inbox.go
@@ -1,16 +1,66 @@
 package srfax
 
 import (
+	"time"
+
 	"github.com/pkg/errors"
 )
 
-// GetFaxInboxOpts contains optional arguments when retrieving inbox items.
-type GetFaxInboxOpts struct {
+// InboxOptions contains optional arguments when retrieving inbox items.
+type InboxOptions struct {
 	Period          string `json:"sPeriod,omitempty"`
 	StartDate       string `json:"sStartDate,omitempty"`
 	EndDate         string `json:"sEndDate,omitempty"`
 	ViewedStatus    string `json:"sViewedStatus,omitempty"`
 	IncludeSubUsers string `json:"sIncludeSubUsers,omitempty"`
+}
+
+func (o *InboxOptions) validate() error {
+
+	if o.Period != "" {
+		switch o.Period {
+		case "RANGE":
+			if ok := validDates("20060102", o.StartDate, o.EndDate); !ok {
+				return errors.New("when Period set to RANGE must supply StartDate and EndDate; format must be YYYYMMDD")
+			}
+		case "ALL":
+			if o.StartDate != "" || o.EndDate != "" {
+				return errors.New("StartDate or EndDate only required when Period set to RANGE")
+			}
+		default:
+			return errors.New("Period must be ALL|RANGE")
+		}
+
+	}
+
+	if o.IncludeSubUsers != "" && o.IncludeSubUsers != "Y" {
+		return errors.New(`IncludeSubUsers must be blank or set to "Y"`)
+	}
+
+	if o.ViewedStatus != "" {
+		switch o.ViewedStatus {
+		case "UNREAD":
+			break
+		case "READ":
+			break
+		case "ALL":
+			break
+		default:
+			return errors.New("ViewedStatus must be blank or one of READ|UNREAD|ALL")
+		}
+	}
+
+	return nil
+}
+
+func validDates(layout string, values ...string) bool {
+	for _, val := range values {
+		if _, err := time.Parse(layout, val); err != nil {
+			return false
+		}
+	}
+
+	return true
 }
 
 // GetFaxInboxResp represents fax inbox information.
@@ -35,22 +85,23 @@ type GetFaxInboxResp struct {
 type getFaxInboxReq struct {
 	Action string `json:"action"`
 	Client
-	GetFaxInboxOpts
+	InboxOptions
 }
 
 // GetFaxInbox retrieves a list of faxes received for a specified period of time.
-func (c *Client) GetFaxInbox(options ...GetFaxInboxOpts) (*GetFaxInboxResp, error) {
-	opts := GetFaxInboxOpts{}
-	if len(optArgs) >= 1 {
-		opts = optArgs[0]
+func (c *Client) GetFaxInbox(options ...InboxOptions) (*GetFaxInboxResp, error) {
+	opts := InboxOptions{}
 	if len(options) >= 1 {
 		opts = options[0]
+		if err := opts.validate(); err != nil {
+			return nil, err
+		}
 	}
 
 	req := getFaxInboxReq{
-		Action:          actionGetFaxInbox,
-		Client:          *c,
-		GetFaxInboxOpts: opts,
+		Action:       actionGetFaxInbox,
+		Client:       *c,
+		InboxOptions: opts,
 	}
 
 	msg, err := sendPost(req)
