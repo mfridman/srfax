3218667a0e1cdad71de58a9536d4cbf10b037c55
Michael Fridman <michaelfridman@me.com>
2018-02-18 22:16:02 -0500

    Update note about error handling link

--- doc.go
+++ doc.go
@@ -6,7 +6,7 @@ https://www.srfax.com/developers/our-fax-api/
 
 There are a few notable differences between the official API docs and this Go client:
 
-1.	As a result of variable types being returned in the Result field, see note about error handling: https://github.com/mfridman/srfax#srfax
+1.	As a result of variable types being returned in the Result field, see note about error handling: https://github.com/mfridman/srfax#a-note-about-error-handling
 
 2.	Response format supports JSON-only. Despite the API being able to return both XML
 and JSON, it is a design decision to support JSON only. As a result,
